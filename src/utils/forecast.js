cd36d3e103d076337dfb2d111a93646d38cda878
Moses_100 <myaseen@sgssupport.abudhabi.ae>
2020-07-26 22:26:28 +0400

    Add , to the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ request({url, json: true}, (error, {body}) => {
     } else if (body.error){
         callback('Unable to find location',undefined)
     } else{
-        callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ', It feels like ' +    body.current.feelslike)   
+        callback(undefined, body.current.weather_descriptions[0] + ', It is currently ' + body.current.temperature + ', It feels like ' +    body.current.feelslike)   
     }
 })             
 }
